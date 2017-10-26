37397b819877257e3eba9f87b0fd5dd3e7e01a12
stonedong <stonedong@tencent.com>
2017-10-26 14:48:26 +0800

    init

---
+++ Products/EsayMoney/EsayMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EsayMoney
+//
+//  Created by Dong Zhao on 2017/10/26.
+//  Copyright © 2017年 qcloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
